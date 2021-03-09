6e3cc94114cd13e0a5445380a2ff54f89fc71e3a
KAZUMiX <kazumix@gmail.com>
2021-03-09 15:01:51 +0900

    クールダウンの時間をパワーの効果時間と同じにする

---
+++ SmartValheim/SmartValheim.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace SmartValheim
+{
+    [BepInPlugin("jp.refactor.valheim.plugins.SmartValheim", "SmartValheim Plug-In", "1.0.0.0")]
+    public class SmartValheim : BaseUnityPlugin
+    {
+        public static ManualLogSource MyLogSource;
+
+        void Awake()
+        {
+            MyLogSource = new ManualLogSource("SmartValheim");
+            BepInEx.Logging.Logger.Sources.Add(MyLogSource);
+
+            Harmony.CreateAndPatchAll(typeof(SmartValheim));
+
+            MyLogSource.LogInfo("Awaked.");
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(Player), "ActivateGuardianPower")]
+        private static void ActivateGuardianPower_Prefix(Player __instance)
+        {
+            MyLogSource.LogInfo("ActivateGuardianPower_Prefix");
+            // クールダウンの時間をパワーの効果時間と同じにする
+            __instance.m_guardianSE.m_cooldown = __instance.m_guardianSE.m_ttl;
+        }
+    }
+}
